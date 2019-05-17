ab177707c1bb92e605c72425a85917c9c94eed1a
Joseph peculiar <bizmindx@gmail.com>
2019-05-16 23:34:26 -1200

    improved ui/ux feel

--- src/components/workings/index.js
+++ src/components/workings/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Step from "./steps";
+import imgs from "./../../assets/imgs/index";
 import './index.css'
 
 export default () => (
@@ -7,14 +8,20 @@ export default () => (
     <h3>HOW IT WORKS</h3>
     <div className="flex-container step-mobile">
       <Step
+        image={imgs.link}
+        alt="link-account"
         topic="Connect to Metamask"
-        content="We don't expose you to risks by asking"
+        content="We do not expose you to risks by asking for your private keys"
       />
       <Step
+        image={imgs.add}
+        alt="add-address"
         topic="Add Addresses"
         content="Add addresses manually or import from Google sheets"
       />
       <Step
+        image={imgs.send}
+        alt="hit-send"
         topic="Hit 'Send'"
         content="You're good to go. Your Ether or tokens are distributed to all added addresses at once!"
       />
