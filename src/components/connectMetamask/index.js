02e7a04bab2204be2bf05d48ece4c5243aff5d43
eltneg <yorewole@gmail.com>
2019-05-31 22:40:25 +0100

    Add tip to send page
    - add etherscan url to homepage
    - improve css styling
    - implement error modal

--- src/components/connectMetamask/index.js
+++ src/components/connectMetamask/index.js
@@ -1,17 +1,15 @@
-import React, {Component} from "react";
-import "./index.css";
-import imgs from "./../../assets/imgs/index";
-import Button from "../ButtonWithRouter";
-import ethApi from "./../../utils/contractCall/index";
-import { withContext } from "./../../provider/index";
-import Modal from 'react-modal'
-
+import React, { Component } from 'react';
+import './index.css';
+import imgs from './../../assets/imgs/index';
+import Button from '../ButtonWithRouter';
+import ethApi from './../../utils/contractCall/index';
+import { withContext } from './../../provider/index';
+import Modal from 'react-modal';
 
 // const openImportModal = () => {
 //   const { ctx } = this.props;
 //   ctx.handleChange("modalName", "gsheet");
 // };
-
 
 // export default withContext(({ ctx }) => (
 //   <div className="flex-container shadowize board connect-metamask-board">
@@ -32,7 +30,7 @@ import Modal from 'react-modal'
 //           }else{
 //             // return ({this.openImportModal})
 //           }
-         
+
 //           }
 //         }
 //         customStyle="ms-btn"
@@ -43,7 +41,7 @@ import Modal from 'react-modal'
 //   </div>
 // ));
 
- class connectMetamask  extends Component{
+class connectMetamask extends Component {
   state = {
     modalIsOpen: false,
     secondModalIsOpen: false
@@ -70,53 +68,89 @@ import Modal from 'react-modal'
   //   ctx.handleChange("modalName", "gsheet");
   // };
 
-  render(){
+  render() {
     const { ctx } = this.props;
-  return(
-    <div className="flex-container shadowize board connect-metamask-board">
-    <img src={imgs.metamask} alt="metamask-logo" />
-    <div>
-      <h2>Connect to MultiSend</h2>
-      <p>Connect & sign transaction via browser extension</p>
-    </div>
-    <div>
-      <Button
-        to="/send"
-        handleClick={async () => {
-          const ade = await ethApi.checkMetamask();
-          if(ade === false){
-            return this.openModal()
-          } else{
-          const res = await ethApi.enableMetamask();
-          if(res === '1234'){
-            return this.openModal()
-          }else{
+    return (
+      <div className="flex-container shadowize board connect-metamask-board">
+        <img src={imgs.metamask} alt="metamask-logo" />
+        <div>
+          <h2>Connect to MultiSend</h2>
+          <p>Connect & sign transaction via browser extension</p>
+        </div>
+        <div>
+          <Button
+            to="/send"
+            handleClick={async () => {
+              const ade = await ethApi.checkMetamask();
+              if (ade === false) {
+                return this.openModal();
+              } else {
+                const res = await ethApi.enableMetamask();
+                if (res === '1234') {
+                  return this.openModal();
+                } else {
+                  ctx.handleChange('auth', true);
+                  ctx.handleChange('metamaskAddress', res);
 
-          
-          ctx.handleChange("auth", true);
-          ctx.handleChange("metamaskAddress", res);
-          
-          return res;
-          }
-          }
-        }}
-        customStyle="ms-btn"
-      >
-        Connect
-      </Button>
-       <Modal isOpen={this.state.modalIsOpen} ariaHideApp={false} onRequestClose={this.closeModal}>
-           <img src={imgs.error} alt="error"/>
-           <h2>Error in Connection</h2>
-          <div>No web3 provider found. <br/> <span style={{color: '#1bbf0d'}}>For Desktop:</span><br/>
-          please go to <span style={{textDecoration: 'underline'}}>https://metamask.io</span> install metamask and return to this page. <br/>
-          <span style={{color: '#1bbf0d'}}>For Mobile:</span><br/>
-          please go to your device appstore, download <span style={{textDecoration: 'underline'}}>Trustwallet or Status.im</span> open this page from within the app
-          </div>
-          <button onClick={this.closeModal}>Got it</button>
-        </Modal>
-    </div>
-  </div>
-  )
+                  return res;
+                }
+              }
+            }}
+            customStyle="ms-btn"
+          >
+            Connect
+          </Button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            ariaHideApp={false}
+            onRequestClose={this.closeModal}
+          >
+            <img src={imgs.error} alt="error" />
+            <h2>No web3 provider found</h2>
+            <div>
+              Install web3 provider. <br />{' '}
+              <span style={{ color: '#1bbf0d' }}>For Desktop:</span>
+              <br />
+              Please install{' '}
+              <a
+                className="app-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://metamask.io"
+              >
+                Metamask
+              </a>
+              {', '}
+              and return to this page to continue. <br />
+              <span style={{ color: '#1bbf0d' }}>For Mobile:</span>
+              <br />
+              Please go to your device appstore, download{' '}
+              <span>
+                <a
+                  className="app-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://trustwallet.com"
+                >
+                  Trustwallet
+                </a>{' '}
+                or{' '}
+                <a
+                  className="app-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://status.im/get"
+                >
+                  Status.im
+                </a>
+              </span>{' '}
+              and open this page from within the app
+            </div>
+            <button onClick={this.closeModal}>Got it</button>
+          </Modal>
+        </div>
+      </div>
+    );
+  }
 }
-}
-export default withContext(connectMetamask )
+export default withContext(connectMetamask);
